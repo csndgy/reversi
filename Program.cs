ed1a2b1206bf14f191439c6c60e169782761b35d
csndgy <73069087+csndgy@users.noreply.github.com>
2023-10-25 10:30:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using reversi;
+
+Tabla ujTabla = new Tabla("allas.txt");
+
+ujTabla.Megjelenit();
+Console.WriteLine();
+Console.WriteLine("6.feladat");
+Console.WriteLine();
+Console.WriteLine("Kék korongok száma: " + ujTabla.MezokSzama('K'));
+Console.WriteLine("Fehér korongok száma: " + ujTabla.MezokSzama('F'));
+Console.WriteLine("Üres korongok szám: " + ujTabla.MezokSzama('#'));
+Console.WriteLine();
+
+string parameterek = "F;4;1;0;1";
+Console.WriteLine($"[jatekos; sor; oszlop; iranySor; iranyOszlop] = {parameterek}");
+
+var elemek = parameterek.Split(',');
+var j = char.Parse(elemek[0]);
+var s = byte.Parse(elemek[1]);
+var o = byte.Parse(elemek[2]);
+var iSor = byte.Parse(elemek[3]);
+var iOszl = byte.Parse(elemek[4]);
+
+if (ujTabla.VanForditas(j,s,o,iSor,iOszl))
+{
+    Console.WriteLine("Van fordítás!");
+}
+else
+{
+    Console.WriteLine("nincs fordítás!");
+}
---
+++ Tabla.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.WebSockets;
+
+namespace reversi
+{
+    public class Tabla
+    {
+        char[,] t;
+        public Tabla(string fajlBeolv)
+        {
+            t = new char[8, 8];
+
+            string[] beolvasas = File.ReadAllLines(fajlBeolv).ToArray();
+
+            for (int i = 0; i < beolvasas.Length; i++)
+            {
+
+                for (int j = 0; j < beolvasas[i].Length; j++)
+                {
+                    t[i, j] = beolvasas[i][j];
+                }
+            }
+
+
+
+        }
+        public void Megjelenit()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write(t[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public byte MezokSzama(char c)
+        {
+            byte darab = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (t[i, j] == c)
+                    {
+                        darab++;
+                    }
+                }
+            }
+            return darab;
+        }
+
+        public bool VanForditas(char jatekos, byte sor, byte oszlop, byte iranySor, byte iranyOszlop)
+        {
+            int aktSor, aktOszlop;
+            char ellenfel;
+            bool nincsEllenfel;
+            aktSor = sor + iranySor;
+            aktOszlop = oszlop + iranyOszlop;
+            ellenfel = 'K';
+            if (jatekos == 'K')
+            {
+                ellenfel = 'F';
+            }
+            nincsEllenfel = true;
+
+            while (aktSor > 0 && aktSor < 8 && aktOszlop > 0 && aktOszlop < 8 && t[aktSor, aktOszlop] == ellenfel)
+            {
+                aktSor = aktSor + iranySor;
+                aktOszlop = aktOszlop + iranyOszlop;
+                nincsEllenfel = false;
+            }
+            if (nincsEllenfel || aktSor < 0 || aktSor > 7 || aktOszlop < 0 || aktOszlop > 7 || t[aktSor, aktOszlop] != jatekos)
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+        public bool IsSzabalyos(char jatekos, byte sor, byte oszlop)
+        {
+            
+            if (t[sor,oszlop] =='#')
+            {
+                bool jo = false;
+
+                //2 forciklus -1tol +1-ig iranysor iranyoszlop forditas ha 1x is volt akkor a jo valtozot true
+
+
+
+                Console.WriteLine("Szabályos lépés");
+                return jo;
+            }
+            else
+            {
+                Console.WriteLine("Szabálytalan lépés!");
+                return false;
+            }
+        }
+    }
+}
